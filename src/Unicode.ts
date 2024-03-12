bd66faba409f7178d72bcc86e19caeda1a4bda02
miguelkjesus <miguelkjesus05@gmail.com>
2024-03-12 17:03:54 +0000

    Added lots of unicode symbols

---
+++ src/Unicode.ts
@@ -0,0 +1,154 @@
+// https://wiki.bedrock.dev/concepts/emojis.html#custom-emoji
+
+export enum Unicode {
+  // HUD
+  Food = "\uE100",
+  Armor = "\uE101",
+  Heart = "\uE10C",
+
+  // Items & Blocks
+  WoodenPickaxe = "\uE108",
+  WoodenSword = "\uE109",
+  CraftingTable = "\uE10A",
+  Furnace = "\uE10B",
+
+  // Marketplace
+  Minecoin = "\uE102",
+  Token = "\uE105",
+
+  // Inventory
+  CraftingToggledOn = "\uE0A0",
+  CraftingToggledOff = "\uE0A1",
+
+  // New Touch Controls
+  NewTouchJump = "\uE014",
+  NewTouchAttack = "\uE015",
+  NewTouchJoystick = "\uE016",
+  NewTouchPlace = "\uE018",
+  NewTouchSneak = "\uE019",
+  NewTouchSprint = "\uE01A",
+  NewTouchFlyUp = "\uE01B",
+  NewTouchFlyDown = "\uE01C",
+  NewTouchDismount = "\uE01D",
+
+  // Old Touch Controls
+  TouchUpArrow = "\uE080",
+  TouchLeftArrow = "\uE081",
+  TouchDownArrow = "\uE082",
+  TouchRightArrow = "\uE083",
+  TouchJump = "\uE084",
+  TouchCrouch = "\uE085",
+  TouchFlyUp = "\uE086",
+  TouchFlyDown = "\uE087",
+  TouchStopFlying = "\uE088",
+
+  SmallTouchUpArrow = "\uE055",
+  SmallTouchLeftArrow = "\uE056",
+  SmallTouchDownArrow = "\uE057",
+  SmallTouchRightArrow = "\uE058",
+  SmallTouchJump = "\uE059",
+  SmallTouchCrouch = "\uE05A",
+  SmallTouchInventory = "\uE05B",
+  SmallTouchFlyUp = "\uE05C",
+  SmallTouchFlyDown = "\uE05D",
+
+  // Keyboard & Mouse Controls
+  LeftClick = "\uE060",
+  RightClick = "\uE061",
+  MiddleClick = "\uE062",
+
+  SmallLeftClick = "\uE070",
+  SmallRightClick = "\uE071",
+  SmallMiddleClick = "\uE073",
+  SmallMouse = "\uE074",
+
+  // Xbox Controls
+  XboxA = "\uE000",
+  XboxB = "\uE001",
+  XboxX = "\uE002",
+  XboxY = "\uE003",
+  XboxLB = "\uE004",
+  XboxRB = "\uE005",
+  XboxLT = "\uE006",
+  XboxRT = "\uE007",
+  XboxBack = "\uE008",
+  XboxStart = "\uE009",
+  XboxLS = "\uE00A",
+  XboxRS = "\uE00B",
+  XboxDPadUp = "\uE00C",
+  XboxDPadLeft = "\uE00D",
+  XboxDPadDown = "\uE00E",
+  XboxDPadRight = "\uE00F",
+
+  // Nintendo Switch Controls
+  SwitchA = "\uE040",
+  SwitchB = "\uE041",
+  SwitchX = "\uE042",
+  SwitchY = "\uE043",
+  SwitchLB = "\uE044",
+  SwitchRB = "\uE045",
+  SwitchZL = "\uE046",
+  SwitchZR = "\uE047",
+  SwitchMinus = "\uE048",
+  SwitchPlus = "\uE049",
+  SwitchLS = "\uE04A",
+  SwitchRS = "\uE04B",
+  SwitchDPadUp = "\uE04C",
+  SwitchDPadLeft = "\uE04D",
+  SwitchDPadDown = "\uE04E",
+  SwitchDPadRight = "\uE04F",
+
+  // Playstation Controls
+  PSCross = "\uE020",
+  PSCircle = "\uE021",
+  PSSquare = "\uE022",
+  PSTriangle = "\uE023",
+  PSL1 = "\uE024",
+  PSR1 = "\uE025",
+  PSL2 = "\uE026",
+  PSR2 = "\uE027",
+  PSTouchPad = "\uE028",
+  PSOptions = "\uE029",
+  PSL3 = "\uE02A",
+  PSR3 = "\uE02B",
+  PSDPadUp = "\uE02C",
+  PSDPadLeft = "\uE02D",
+  PSDPadDown = "\uE02E",
+  PSDPadRight = "\uE02F",
+
+  // Oculus Controls
+  Oculus0 = "\uE0E0",
+  OculusA = "\uE0E1",
+  OculusB = "\uE0E2",
+  OculusLG = "\uE0E3",
+  OculusRG = "\uE0E4",
+  OculusLS = "\uE0E5",
+  OculusRS = "\uE0E6",
+  OculusLT = "\uE0E7",
+  OculusRT = "\uE0E8",
+  OculusX = "\uE0E9",
+  OculusY = "\uE0EA",
+
+  // Windows MR Controls
+  WindowsMRLG = "\uE0C0",
+  WindowsMRRG = "\uE0C1",
+  WindowsMRMenu = "\uE0C2",
+  WindowsMRLS = "\uE0C3",
+  WindowsMRRS = "\uE0C4",
+  WindowsMRLeftTouchpad = "\uE0C5",
+  WindowsMRLeftHorizontalTouchpad = "\uE0C6",
+  WindowsMRLeftVerticalTouchpad = "\uE0C7",
+  WindowsMRRightTouchpad = "\uE0C8",
+  WindowsMRRightHorizontalTouchpad = "\uE0C9",
+  WindowsMRRightVerticalTouchpad = "\uE0CA",
+  WindowsMRLT = "\uE0CB",
+  WindowsMRRT = "\uE0CC",
+  WindowsMRWindows = "\uE0CD",
+
+  // Other
+  Crosshair = "\uE017",
+  Agent = "\uE103",
+  ImmersiveReader = "\uE104",
+  HollowStar = "\uE106",
+  SolidStar = "\uE107",
+}
