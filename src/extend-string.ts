01df864c4704c6dfe257ddcfb6f593daf9b6c3eb
miguelkjesus <miguelkjesus05@gmail.com>
2024-06-05 11:51:05 +0100

    Refactor string extension to minimize side effects
    - Implemented tracking of modified attributes in the String prototype.
    - `extend()` and `unextend()` now have minimal side effects when used with other libraries that modify the String prototype.

--- src/extend-string.ts
+++ src/extend-string.ts
@@ -1,6 +1,9 @@
 import { StyledString } from "./StyledString.js";
 import { StyleCode } from "./StyleCode.js";
 
+let originalProperties = {};
+let overwrittenProperties = {};
+
 /**
  * Adds the styling methods to the String prototype.
  *
@@ -23,12 +26,21 @@ import { StyleCode } from "./StyleCode.js";
  */
 export function extend() {
   for (let [style, code] of Object.entries(StyleCode)) {
-    Object.defineProperty(String.prototype, style, {
+    originalProperties[style] = Object.getOwnPropertyDescriptor(
+      String.prototype,
+      style
+    );
+
+    let prop = {
       get(this: string): StyledString {
         return new StyledString(code, this);
       },
       configurable: true,
-    });
+      enumerable: false,
+    };
+
+    Object.defineProperty(String.prototype, style, prop);
+    overwrittenProperties[style] = prop;
   }
 }
 
@@ -54,6 +66,33 @@ export function extend() {
  */
 export function unextend() {
   for (let style of Object.keys(StyleCode)) {
-    delete String.prototype[style];
+    // Check if it hasnt been overwritten in the time it was last extended
+    let currProp = Object.getOwnPropertyDescriptor(String.prototype, style);
+    let extendedProp = overwrittenProperties[style];
+    console.log("\n" + style);
+    if (isPropertyDescriptorEqual(currProp, extendedProp)) {
+      let ogProp = originalProperties[style];
+      console.log(ogProp);
+      if (ogProp === undefined) {
+        delete String.prototype[style];
+      } else {
+        Object.defineProperty(String.prototype, style, ogProp);
+      }
+    }
   }
+}
+
+function isPropertyDescriptorEqual(
+  a: PropertyDescriptor,
+  b: PropertyDescriptor
+) {
+  if (a === b) return true;
+  return (
+    a.value === b.value &&
+    a.writable === b.writable &&
+    a.configurable === b.configurable &&
+    a.enumerable === b.enumerable &&
+    a.get === b.get &&
+    a.set === b.set
+  );
 }
